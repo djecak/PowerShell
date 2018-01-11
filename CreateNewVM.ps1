db6bb5f6a0ed1d63c212e2e9f4431288151bd24c
Ermin Mlinaric <ermin.mlinaric@live.com>
2018-01-11 13:19:47 +1100

    Azure

---
+++ CreateNewVM.ps1
@@ -0,0 +1,105 @@
+﻿#Login to your Azure Account and select subscription:
+
+Login-AzureRmAccount
+$SubscriptionId = (Get-AzureRmSubscription | Out-GridView -Title "Select Azure Subscription..."-PassThru).SubscriptionID
+Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+#Select-AzureRmProfile -Path C:\ZiScript\AzureTest\AzureTestRmProfile.json
+$locName = "australiasoutheast"
+$publisher=(Get-AzureRmVMImagePublisher -Location $locName | OGV -Title "Select Azure Publisher (hint: use 'Add criteria' to filter)..." -passthru).PublisherName #check all the publishers available
+$offer=(Get-AzureRmVMImageOffer -Location $locName -PublisherName $publisher | OGV -Title "Select Azure Offer (hint: use 'Add criteria' to filter)..." -passthru).Offer #look for offers for a publisher
+$osSKU=(Get-AzureRmVMImageSku -Location $locName -PublisherName $publisher -Offer $offer | OGV -Title "Select OS SKU (hint: use 'Add criteria' to filter)..." -passthru).Skus #view SKUs for an offer
+
+$vmSize = (Get-AzureRmVmSize -Location $locName | Select-Object Name, NumberOfCores, MemoryInMB, MaxDataDiskCount, ResourceDiskSizeInMB | OGV -PassThru).Name
+
+$VMcred = Get-Credential -Message "Type the name and password of the VM local administrator account. The password must be at 12-123 characters long and have at least one lower case character, one upper case character, one number, and one special character."
+
+$rgName = read-host "Enter Resource Group name"
+$storaccName = read-host "Enter Storage Account name"
+$vmName = read-host "Enter VM name"
+$stType = "Standard_LRS"
+$subnetName = "$rgName-subnet"
+$vnetName = "$rgName-vnet"
+$compName = $vmName
+$diskName = "$vmName-osdisk"
+$vnetRange = "172.21.0.0/16"
+$SubnetRange = "172.21.0.0/24"
+
+#Create a resource group, skip if already exists:
+
+try {     
+    Get-AzureRmResourceGroup -Name $rgName -Location $locName -ErrorAction Stop     
+    Write-Host 'RG already exists... skipping' -foregroundcolor yellow -backgroundcolor red 
+} catch {     
+  New-AzureRmResourceGroup -Name $rgName -Location $locName 
+}
+
+#Create a storage account (Standard LRS):
+
+
+#Check for storage name availability (needs to be unique in globally) 
+do  {
+$StorAccNameAvail = Get-AzureRmStorageAccountNameAvailability -Name $storaccName
+    if ($StorAccNameAvail.NameAvailable -eq 'True')
+    { New-AzureRmStorageAccount -Name $storaccName -ResourceGroupName $rgName -Type $stType -Location $locName }
+    else
+        {Write-Host -Separator `n
+         Write-Host "The storage account named $storaccName is already taken" -ForegroundColor yellow -BackgroundColor Red
+         Write-Host -Separator `n
+         $storaccName = read-host "Enter new storage account name" 
+         }
+    }
+until ($StorAccNameAvail.NameAvailable -eq 'True')
+$storacct = Get-AzureRmStorageAccount -ResourceGroupName $rgName -StorageAccountName $storaccName
+
+#Create a virtual network and subnet:
+
+$singleSubnet = New-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $SubnetRange
+
+
+#$vnet = New-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName -Location $locName -AddressPrefix 10.0.0.0/16 -Subnet $singleSubnet
+try {     
+    $vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName -ErrorAction Stop     
+    Write-Host 'VNET already exists... skipping' -foregroundcolor yellow -backgroundcolor red 
+} catch {     
+    $vnet = New-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName -Location $locName -AddressPrefix $vnetRange -Subnet $singleSubnet 
+}
+$subnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet
+
+#Create a public IP address and network interface:
+
+$pip = New-AzureRmPublicIpAddress -Name "${vmname}_PIP1" -ResourceGroupName $rgName -Location $locName -AllocationMethod Dynamic
+
+$nic = New-AzureRmNetworkInterface -Name "${vmname}_nic1" -ResourceGroupName $rgName -Location $locName -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id
+
+#Create a virtual machine:
+
+    #set the administrator account name and password for the virtual machine. 
+    #The password must be at 12-123 characters long and have at least one lower case character, one upper case character, one number, and one special character.
+    
+    #Create the variable and the virtual machine configuration
+    $vm = New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize
+    # Create the computer name variable and add the operating system information to the configuration
+    
+    $vm = Set-AzureRmVMOperatingSystem -VM $vm -Windows -ComputerName $compName -Credential $VMcred -ProvisionVMAgent -EnableAutoUpdate
+    #Define the image to use to provision the virtual machine
+    $vm = Set-AzureRmVMSourceImage -VM $vm -PublisherName $publisher -Offer $offer -Skus $osSKU -Version "latest"
+    #Add the network interface that you created to the configuration
+    $vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
+    #Create the variables vhd and disk uri
+    $blobPath = "vhds/$vmName-Disk.vhd"
+    $osDiskUri = $storAcct.PrimaryEndpoints.Blob.ToString() + $blobPath
+    #Create the OS disk variable and add the disk information to the configuration
+    
+    $vm = Set-AzureRmVMOSDisk -VM $vm -Name $diskName -VhdUri $osDiskUri -CreateOption fromImage
+    #Create the VM
+    New-AzureRmVM -ResourceGroupName $rgName -Location $locName -VM $vm
+
+#Get VM's public IP
+$rdpIP = (Get-AzureRmPublicIpAddress -Name $pip.Name -ResourceGroupName $rgName).IpAddress
+
+#$rdpString = $vmName + '.' + $rdpVM.Location + '.cloudapp.azure.com:3389' 
+Get-AzurermRemoteDesktopFile -ResourceGroupName $rgName -Name $vmName -LocalPath "c:\temp\$vmName.rdp"
+
+Write-Host "Azure VM provisioning succeeded!" -foregroundcolor Green
+Write-Host "Connect to the VM using the IP below or using $vmName.rdp file in C:\Temp" -foregroundcolor Green
+Write-Host $rdpIP -foregroundcolor Green
---
+++ CreateNewVMfromExistingVHD.ps1
@@ -0,0 +1,67 @@
+﻿Select-AzureRmProfile -Path C:\ZiScript\AzureTest\AzureTestRmProfile.json
+
+$imageURI = "https://mystoragewin10test.blob.core.windows.net/vhds/AzureSRV1.vhd"
+
+#Location and RG for new VM
+$locName = "australiasoutheast"
+$rgName = "mygroup1"
+
+
+#Network info:
+$subnetName = "mysubnet1"
+$vnetName = "myvnet1"
+$vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
+#Create new NIC and Public IP
+$ipName = "myIPaddress2"
+$pip = New-AzureRmPublicIpAddress -Name $ipName -ResourceGroupName $rgName -Location $locName -AllocationMethod Dynamic
+
+$nicName = "mynic2"
+$nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -Location $locName -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id
+
+    # Enter a new user name and password to use as the local administrator account 
+    # for remotely accessing the VM.
+    $cred = Get-Credential
+
+    # Name of the storage account where the VHD is located. This example sets the 
+    # storage account name as "myStorageAccount"
+    $storageAccName = "mystoragewin10test"
+
+    # Name of the virtual machine. This example sets the VM name as "myVM".
+    $vmName = "Win10VHD"
+
+    # Size of the virtual machine. This example creates "Standard_D2_v2" sized VM. 
+    # See the VM sizes documentation for more information: 
+    # https://azure.microsoft.com/documentation/articles/virtual-machines-windows-sizes/
+    $vmSize = "Standard_D2_v2"
+
+    # Computer name for the VM. This examples sets the computer name as "myComputer".
+    $computerName = "Win10VHD"
+
+    # Name of the disk that holds the OS. This example sets the 
+    # OS disk name as "myOsDisk"
+    $osDiskName = "Win10_OsDisk"
+
+    # Assign a SKU name. This example sets the SKU name as "Standard_LRS"
+    # Valid values for -SkuName are: Standard_LRS - locally redundant storage, Standard_ZRS - zone redundant storage, Standard_GRS - geo redundant storage, Standard_RAGRS - read access geo redundant storage, Premium_LRS - premium locally redundant storage. 
+    $skuName = "Standard_LRS"
+
+    # Get the storage account where the uploaded image is stored
+    $storageAcc = Get-AzureRmStorageAccount -ResourceGroupName $rgName -AccountName $storageAccName
+
+    # Set the VM name and size
+    $vmConfig = New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize
+
+    #Set the Windows operating system configuration and add the NIC
+    $vm = Set-AzureRmVMOperatingSystem -VM $vmConfig -Windows -ComputerName $computerName -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
+
+    $vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
+
+    # Create the OS disk URI
+    $osDiskUri = '{0}vhds/{1}-{2}.vhd' -f $storageAcc.PrimaryEndpoints.Blob.ToString(), $vmName.ToLower(), $osDiskName
+
+    # Configure the OS disk to be created from the existing VHD image (-CreateOption fromImage).
+
+    $vm = Set-AzureRmVMOSDisk -VM $vm -Name $osDiskName -VhdUri $osDiskUri -CreateOption fromImage -SourceImageUri $imageURI -Windows
+
+    # Create the new VM
+    New-AzureRmVM -ResourceGroupName $rgName -Location $locName -VM $vm
